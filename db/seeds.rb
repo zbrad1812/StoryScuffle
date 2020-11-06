163f39b0fe843b6fb28c6c5170d05e849520ac29
Zach Bradford <zbrad1800@Zachs-MacBook-Air.local>
2020-11-06 15:13:50 -0500

    api-endpoints

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Story.destroy_all #Why is this so dramatic?
+
+Story.create!(title: "The Horror of Virginia", description: "A spectre haunts a small town in Virginia...", content: "Okay so one day there was this guy and he went to a place and some conflict happened and it was kind of scary for a bit but it got resolved in a pretty satisfying way, it was pretty unique and crazy")
+Story.create!(title: "The Scarytime of Maryland", description: "A spectre haunts a small town in Maryland...", content: "Okay so one day there was this guy and he went to a place and some conflict happened and it was kind of scary for a bit but it got resolved in a pretty satisfying way, it was pretty unique and crazy")
+Story.create!(title: "The Nasty Ghost Guy of Massachusetts", description: "A spectre haunts a small town in Massachusetts...", content: "Okay so one day there was this guy and he went to a place and some conflict happened and it was kind of scary for a bit but it got resolved in a pretty satisfying way, it was pretty unique and crazy")
+Story.create!(title: "The Freaky Fright of South Carolina", description: "A spectre haunts a small town in South Carolina...", content: "Okay so one day there was this guy and he went to a place and some conflict happened and it was kind of scary for a bit but it got resolved in a pretty satisfying way, it was pretty unique and crazy")
+Story.create!(title: "The Horror of North Dakota", description: "A spectre haunts a small town in North Dakota...", content: "Okay so one day there was this guy and he went to a place and some conflict happened and it was kind of scary for a bit but it got resolved in a pretty satisfying way, it was pretty unique and crazy")
