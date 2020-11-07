bafeae38a70b31b68f462dcb97281f6bd1deb9cd
Zach Bradford <zbrad1800@Zachs-MacBook-Air.local>
2020-11-07 15:14:44 -0500

    functional-show-page

--- app/javascript/react/components/StoriesShowPage.js
+++ app/javascript/react/components/StoriesShowPage.js
@@ -1,12 +1,34 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 const StoriesShowPage = (props) => {
   let id = props.match.params.id
+  let [story, setStory] = useState({})
 
-  
-  debugger
+  useEffect(() => {
+    fetch(`/api/v1/stories/${id}`)
+      .then((response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          let errorMessage = `${response.status} (${response.statusText})`,
+            error = new Error(errorMessage);
+          throw error;
+        }
+      })
+      .then((response) => response.json())
+      .then((body) => {
+        setStory(body);
+      })
+      .catch((error) => console.error(`Error in fetch: ${error.message}`));
+  }, []);
+
+
   return(
-    <h1>schtinky</h1>
+    <>
+      <h2> {story.title} </h2>
+      <h3> {story.description} </h3>
+      <p> {story.content} </p>
+    </>
   )
 }
 
--- app/javascript/react/components/Story.js
+++ app/javascript/react/components/Story.js
@@ -1,19 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Story = (props) => {
-if (props.story !== undefined) {
-  return (
-    <>
-      <h1> {props.story.title} </h1>
-      <h4> {props.story.description} </h4>
-    </>
-  )
-} else {
-  return (
-    <>
-    </>
-  );
-}
+  let id = props.story.id
+  if (props.story !== undefined) {
+    return (
+      <>
+        <Link to={`stories/${id}`}>
+          <h3> {props.story.title} </h3>
+        </Link>
+        <h4> {props.story.description} </h4>
+      </>
+    )
+  } else {
+    return (
+      <>
+      </>
+    );
+  }
 };
 
 export default Story
