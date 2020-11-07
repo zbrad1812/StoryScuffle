10b1641443a444a49629893b55ea30de2a0fb5b3
Zach Bradford <zbrad1800@Zachs-MacBook-Air.local>
2020-11-07 14:39:32 -0500

    API-show-and-index-page

---
+++ app/javascript/react/components/StoriesShowPage.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const StoriesShowPage = (props) => {
+  let id = props.match.params.id
+
+  
+  debugger
+  return(
+    <h1>schtinky</h1>
+  )
+}
+
+export default StoriesShowPage
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   root 'homes#index'
+
+  get '/stories', to: 'homes#index'
+  get '/stories/:id', to: 'homes#index'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :stories, only: [:index]
+      resources :stories, only: [:index, :show]
     end
   end
 end
