215a0a33a8357384ab5f8bf789694f44fa16d8fd
Zach Bradford <zbradford1812@gmail.com>
2021-02-12 13:06:40 -0500

    edited about page

--- app/javascript/react/components/AboutPage.js
+++ app/javascript/react/components/AboutPage.js
@@ -3,14 +3,10 @@ import React from "react"
 const AboutPage = (props) => {
 
   return(
-    <h4 className="about-paragraph"> Thank you so much for reading this and visiting my website :) 
+    <h4 className="about-paragraph"> Thank you for visiting my website :) 
     this is the very first website that I built all on my own (with 
     the help of some good programmer friends here and there) so please
-    try to go a little easy on me ;)
-    
-    Also, this site is still very much under construction. I am learning more
-    and more every day and I'm really looking to refactor and add a lot of features.
-    I hope you have a good day!</h4>
+    go a little easy on me. I hope you have a good day!</h4>
   )
 }
 
